a1abb4afc40d313f7212127385786fb676ed8179
Nir Bar <nir.bar@panel-sw.co.il>
2023-06-05 13:34:37 +0300

    ApplyCommit indicates restart if needed

--- src/ext/Bal/stdbas/WixStandardBootstrapperApplication.cpp
+++ src/ext/Bal/stdbas/WixStandardBootstrapperApplication.cpp
@@ -1376,6 +1376,10 @@ public: // IBootstrapperApplication
 
         // Automatically restart if we're not displaying a UI or the command line said to always allow restarts.
         m_fAllowRestart = m_fShouldRestart && (BOOTSTRAPPER_DISPLAY_FULL > m_commandDisplay || BAL_INFO_RESTART_PROMPT < m_BalInfoCommand.restart);
+        if ((*pAction == BOOTSTRAPPER_APPLYCOMPLETE_ACTION_RESTART) && !m_fAllowRestart)
+        {
+            *pAction = BOOTSTRAPPER_APPLYCOMPLETE_ACTION_NONE;
+        }
 
         if (m_fPrereq)
         {
@@ -1390,8 +1394,6 @@ public: // IBootstrapperApplication
 
         SetState(WIXSTDBA_STATE_APPLIED, hrStatus);
         SetTaskbarButtonProgress(100); // show full progress bar, green, yellow, or red
-
-        *pAction = BOOTSTRAPPER_APPLYCOMPLETE_ACTION_NONE;
 
         return hr;
     }
