9a59f27bc2a90e45c930bde885aba4ab7867507d
Nir Bar <nir.bar@panel-sw.co.il>
2024-09-17 11:50:12 +0300

    Create containers concurrently

--- src/wix/WixToolset.Core.Burn/Bundles/CreateNonUXContainers.cs
+++ src/wix/WixToolset.Core.Burn/Bundles/CreateNonUXContainers.cs
@@ -7,6 +7,7 @@ namespace WixToolset.Core.Burn.Bundles
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
+    using System.Threading.Tasks;
     using WixToolset.Data;
     using WixToolset.Data.Burn;
     using WixToolset.Data.Symbols;
@@ -128,10 +129,13 @@ namespace WixToolset.Core.Burn.Bundles
 
             if (!this.Messaging.EncounteredError)
             {
+                List<Task> containerTasks = new List<Task>();
                 foreach (var container in this.Containers.Where(c => !String.IsNullOrEmpty(c.WorkingPath) && c.Id.Id != BurnConstants.BurnUXContainerName))
                 {
-                    this.CreateContainer(container, payloadsByContainer[container.Id.Id]);
+                    Task task = Task.Run(() => this.CreateContainer(container, payloadsByContainer[container.Id.Id]));
+                    containerTasks.Add(task);
                 }
+                Task.WaitAll(containerTasks.ToArray());
             }
 
             this.UXContainerPayloads = uxPayloadSymbols;
